6946310e5ba4164c04d1e4e0ab10c37ee6c661e1
Malik_Babu <94501981+PyBabu@users.noreply.github.com>
2025-07-22 17:06:27 +0530

    Update settings.py

--- collab_editor/settings.py
+++ collab_editor/settings.py
@@ -127,8 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR , 'static']
+import os
+
+STATIC_URL = '/static/'
+# This is where collectstatic will collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
